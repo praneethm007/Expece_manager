0c16df3410adb66084783f5ef8e4550ef70d5df4
Praneeth <praneethreddym007@gmail.com>
2021-11-07 01:20:00 +0530

    backbutton

--- expense_manager/src/app/add-expense/add-expense.page.ts
+++ expense_manager/src/app/add-expense/add-expense.page.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-expense.page.scss'],
 })
 export class AddExpensePage implements OnInit {
+  namee ='good ';
 
   constructor() { }
 
