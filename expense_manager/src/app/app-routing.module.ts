816d4cbc757aa6af816f5a01195b06688381cad7
mpreethi536 <madavapeddi.preethi@gmail.com>
2021-11-06 22:51:25 +0530

    Added expense page

--- expense_manager/src/app/app-routing.module.ts
+++ expense_manager/src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'add-expense',
+    loadChildren: () => import('./add-expense/add-expense.module').then( m => m.AddExpensePageModule)
+  },
 ];
 
 @NgModule({
